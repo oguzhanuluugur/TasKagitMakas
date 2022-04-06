a55d8c7f010082afd0d9f3e0ebb7823af53a7f7a
Oğuzhan Uluuğur <oguzhanuluugur345@gmail.com>
2022-04-06 14:24:26 +0300

    C# form ile taş kağıt makas oyunu yapımı

---
+++ TasKagitMakas/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace TasKagitMakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(306, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Makas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(306, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 68);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Kağıt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(316, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 71);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Taş";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(317, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 64);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Taş";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(306, 82);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 76);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Kağıt";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Enabled = false;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(306, 155);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(164, 64);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Makas";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(51, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 50);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(316, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 62);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(45, 414);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 50);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(782, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 50);
+            this.label4.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(806, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ TasKagitMakas/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasKagitMakas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int skor = 0;
+        int bot = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1, 4);
+            if (deger == 1)
+            {
+                bot++;
+                label4.Text = bot.ToString();
+                label2.Text = "Kaybettiniz";
+                button4.BackColor = Color.Red;
+            }
+            else if (deger == 2)
+            {
+                skor++;
+                label1.Text = skor.ToString();
+                label2.Text = "Kazandınız";
+                button5.BackColor = Color.Green;
+               
+            }
+            else if (deger == 3)
+            {
+                label2.Text = "Beraberlik";
+                button6.BackColor = Color.Gray;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1,4);
+            if (deger == 1)
+            {
+                label2.Text = "Beraberlik";
+                button4.BackColor = Color.Gray;
+            }
+            else if (deger == 2)
+            {
+                bot++;
+                label4.Text = bot.ToString();
+                label2.Text = "Kaybettiniz";
+                button5.BackColor = Color.Red;
+            }
+            else if (deger == 3)
+            {
+                skor++;
+                label1.Text = skor.ToString();
+                label2.Text = "Kazandınız";
+                button6.BackColor = Color.Green;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            Random random = new Random();
+            int deger;
+            deger = random.Next(1, 4);
+            if (deger == 1)
+            {
+                skor++;
+                label1.Text = skor.ToString();
+                label2.Text = "Kazandınız";
+                button4.BackColor = Color.Green;
+            }
+            else if (deger == 2)
+            {
+                label2.Text = "Beraberlik";
+                button5.BackColor = Color.Gray;
+            }
+            else if (deger == 3)
+            {
+                bot++;
+                label4.Text = bot.ToString();
+                label2.Text = "Kaybettiniz";
+                button6.BackColor = Color.Red;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (bot == 3)
+            {
+                label2.Text = "Bilgisayar Kazandı";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else if (skor == 3)
+            {
+                label2.Text = "Oyuncu Kazandı";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+    }
+}
